5dac24f126cfdf432339443c11d504aa1262e14b
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-30 23:27:25 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,7 +8,11 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+    if BRANDS.include?(brand)
+      self 
+    else 
+      BRANDS << brand 
+    end 
   end 
 
   def cobble
